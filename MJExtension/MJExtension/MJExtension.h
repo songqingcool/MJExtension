58c2d81c452cde3853b68cb7dfb851cd3dd54390
songqinggong <songqinggong@syswin.com>
2016-09-28 10:56:53 +0800

    源代码上传

---
+++ MJExtension/MJExtension/MJExtension.h
@@ -0,0 +1,30 @@
+//
+//  MJExtension.h
+//  MJExtension
+//
+//  Created by SONGQG on 2016/9/28.
+//  Copyright © 2016年 思源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJExtension.
+FOUNDATION_EXPORT double MJExtensionVersionNumber;
+
+//! Project version string for MJExtension.
+FOUNDATION_EXPORT const unsigned char MJExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJExtension/PublicHeader.h>
+
+
+//  代码地址:https://github.com/CoderMJLee/MJExtension
+//  代码地址:http://code4app.com/ios/%E5%AD%97%E5%85%B8-JSON-%E4%B8%8E%E6%A8%A1%E5%9E%8B%E7%9A%84%E8%BD%AC%E6%8D%A2/5339992a933bf062608b4c57
+
+#import <MJExtension/NSObject+MJCoding.h>
+#import <MJExtension/NSObject+MJProperty.h>
+#import <MJExtension/NSObject+MJClass.h>
+#import <MJExtension/NSObject+MJKeyValue.h>
+#import <MJExtension/NSString+MJExtension.h>
+#import <MJExtension/MJExtensionConst.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MJExtension
-ww
+MJExtension的简单封装framework静态库
+
+MJExtension源代码下载日期 2016年09月28日10:54:28
+
+clone项目之后直接运行MJExtensionAgg即可得到编译好的MJExtension.framework(真机和模拟器)
+
+1、iOS8以上
+
+2、支持bitcode
+
